da711c9ebe9ae474d5c447697ae64cd734288aac
Prapti Chavan <157265580+PraptiChavan@users.noreply.github.com>
2026-03-04 10:14:24 +0530

    Rename migration class to avoid duplicate declaration

--- database/migrations/2026_03_03_175045_add_sections_column_to_classes_table.php
+++ database/migrations/2026_03_03_175045_add_sections_column_to_classes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddSectionsColumnToClassesTableV2 extends Migration
+return new class extends Migration
 {
     public function up()
     {
@@ -19,4 +19,4 @@ class AddSectionsColumnToClassesTableV2 extends Migration
             $table->dropColumn('sections');
         });
     }
-}
+};
